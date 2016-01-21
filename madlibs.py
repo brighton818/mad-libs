16de994707b80c2e5da1900b6549464efaf15c6d
BrightonKim <brightonkim@seoulchristian.org>
2016-01-21 22:37:31 +0900

    Fix syntax errors

--- madlibs.py
+++ madlibs.py
@@ -4,21 +4,21 @@ def madlibs():
     print "You should enter some information to make this game work."
 
     # Make the player input the variables in order to output later.
-    adjective_1 = raw_input ("Enter an adjective\n> ")
-    verb_1 = raw_input ("Enter an 'ing' verb\n> ")
-    noun = raw_input ("Enter a noun\n> ")
-    verb_2 = raw_input ("Enter a plural present tense verb\n> ")
-    place = raw_input ("Enter a place\n> ")
-    person_name = raw_input ("Enter a person's name\n> ")
-    adjective_2 = raw_input ("Enter an adjective\n> ")
+    adj_1 = raw_input("Enter an adjective\n> ")
+    v_1 = raw_input("Enter an 'ing' verb\n> ")
+    n = raw_input("Enter a noun\n> ")
+    v_2 = raw_input("Enter a plural present tense verb\n> ")
+    place = raw_input("Enter a place\n> ")
+    name = raw_input("Enter a person's name\n> ")
+    adj_2 = raw_input("Enter an adjective\n> ")
 
-    # Printing out the whole story including the inputs that the players had entered
+    # Printing out the story including the inputs that the players had entered
     print """    I am a/an %s SCS student. I am proud of myself!
 I always enjoy %s at school. I will introduce my friend.
 This is my best friend %s. We always %s together.
-When we do that, we go to %s.
-    My favorite teacher is %s. He/she is very %s.
-I love him/her because of this attribute.""" %(adjective_1, verb_1, noun, verb_2, place, person_name, adjective_2)
+When we do that, we go to %s.""" % (adj_1, v_1, n, v_2, place)
+    print """    My favorite teacher is %s. He/she is very %s.
+I love him/her because of this attribute.""" % (name, adj_2)
 
 # Starts the function
 madlibs()
